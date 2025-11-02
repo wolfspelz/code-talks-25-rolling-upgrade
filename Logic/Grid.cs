5a618faddbd96b496feb8b41a83489dbedac89dd
hwolf <hwolf@tegtmeier-inkubator.de>
2025-11-02 16:00:42 +0100

    recreate grid only on size change otherwise update

--- Logic/Grid.cs
+++ Logic/Grid.cs
@@ -12,12 +12,12 @@ public class Grid : Grain, IGrid
     public async Task<FieldProperties[][]> Get(int nx, int ny) {
         var id = this.GetPrimaryKeyString();
 
-        FieldProperties[][] result = new FieldProperties[nx][];
-        for (int ix = 0; ix < nx; ix++) {
-            result[ix] = new FieldProperties[ny];
-            for (int iy = 0; iy < ny; iy++) {
+        FieldProperties[][] result = new FieldProperties[ny][];
+        for (int iy = 0; iy < ny; iy++) {
+            result[iy] = new FieldProperties[nx];
+            for (int ix = 0; ix < nx; ix++) {
                 var grain = GrainFactory.GetGrain<IField>($"{id}-{ix}-{iy}");
-                result[ix][iy] = await grain.Get();
+                result[iy][ix] = await grain.Get();
             }
         }
         return result;
