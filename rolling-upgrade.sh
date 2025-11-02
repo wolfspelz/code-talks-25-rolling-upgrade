edbfe7ddbf4b090d0aed37c5b670cbf9ef9c0224
hwolf <hwolf@tegtmeier-inkubator.de>
2025-11-02 16:11:36 +0100

    dockor compose stop/start instead of stop/rm/up

--- rolling-upgrade.sh
+++ rolling-upgrade.sh
@@ -11,8 +11,7 @@ SLEEP_AFTER_START_SEC=${SLEEP_AFTER_START_SEC:-0}
 for i in $(seq 1 $((NUM_SILOS))); do
   echo "Upgrading node $i..."
   docker compose stop node$i
-  docker compose rm -f node$i
   sleep $SLEEP_AFTER_STOP_SEC
-  docker compose up -d node$i
+  docker compose start node$i
   sleep $SLEEP_AFTER_START_SEC
 done
